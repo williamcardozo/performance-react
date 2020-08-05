c7b489d054968dc72f72454e0043780a61685e2c
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2020-08-05 01:52:00 -0300

    removing react profiler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, { Profiler } from 'react';
+import React from 'react';
 import { ExemploMemoScreen } from './screens'
 
 function App() {
 
-  function onRenderCallback(
-    id, // o prop "id" da árvore Profiler que acabou de atualizar 
-    phase, //  "mount" (se a árvore acabou de ser montada) ou "update" (se foi renderizada novamente)
-    actualDuration, // tempo gasto renderizando a atualização enviada
-    baseDuration, // tempo estimado para renderizar totalmente a subárvore sem memorização
-    startTime, // quando o React começou renderizar essa atualização
-    commitTime, // quando o React enviou essa atualização
-    interactions // um Set de interações pertencentes â essa atualização
-  ) {
-    console.log(actualDuration)
-  }
-
   return (
-    <Profiler id="App" onRender={onRenderCallback}>
-      <ExemploMemoScreen />
-    </Profiler>
+    <ExemploMemoScreen />
   );
 }
 
